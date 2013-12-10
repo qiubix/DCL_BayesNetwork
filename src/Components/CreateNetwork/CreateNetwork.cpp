d68e46b0625f0e5d5ab5dc28b2b7907c63a75ba0
qiubix <qiubix@gmail.com>
2013-12-10 11:04:36 +0100

    Added bare CreateNetwork class files.
    Fixed CMakeLists.txt
    Added CreateNetwork class with basic component methods, still uninitialised,
    without proper datastreams configured.

---
+++ src/Components/CreateNetwork/CreateNetwork.cpp
@@ -0,0 +1,77 @@
+/*!
+ * \file CreateNetwork.cpp
+ * \brief
+ */
+
+#include <memory>
+#include <string>
+#include <iostream>
+
+#include "CreateNetwork.hpp"
+
+#include "Logger.hpp"
+#include "Common/Timer.hpp"
+
+#include <boost/thread.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+namespace Network {
+namespace CreateNetwork {
+
+CreateNetwork::CreateNetwork(const std::string & name) : Base::Component(name)
+{
+    LOG(LTRACE)<<"Hello CreateNetwork\n";
+}
+
+CreateNetwork::~CreateNetwork()
+{
+    LOG(LTRACE)<<"Good bye CreateNetwork\n";
+}
+
+void CreateNetwork::prepareInterface()
+{
+    LOG(LTRACE) << "CreateNetwork::prepareInterface\n";
+}
+
+bool CreateNetwork::onInit()
+{
+    LOG(LTRACE) << "CreateNetwork::initialize\n";
+
+    return true;
+}
+
+bool CreateNetwork::onFinish()
+{
+    LOG(LTRACE) << "CreateNetwork::finish\n";
+
+    return true;
+}
+
+bool CreateNetwork::onStop()
+{
+    LOG(LTRACE) << "CreateNetwork::onStop\n";
+
+    return true;
+}
+
+bool CreateNetwork::onStart()
+{
+    LOG(LTRACE) << "CreateNetwork::onStart\n";
+
+    return true;
+}
+
+bool CreateNetwork::onStep()
+{
+    LOG(LTRACE) << "CreateNetwork::onStep\n";
+
+    return true;
+}
+
+
+void CreateNetwork::onNewImage() {
+    LOG(LTRACE) << "CreateNetwork::onNewImage\n";
+}
+
+}//: namespace CreateNetwork
+}//: namespace Processors
---
+++ src/Components/CreateNetwork/CreateNetwork.hpp
@@ -0,0 +1,99 @@
+/*!
+ * \file CreateNetwork.hpp
+ * \brief
+ */
+
+#ifndef CREATE_NETWORK_HPP_
+#define CREATE_NETWORK_HPP_
+
+#define CV_NO_BACKWARD_COMPATIBILITY
+
+
+#include "Component_Aux.hpp"
+#include "Component.hpp"
+#include "DataStream.hpp"
+#include "Property.hpp"
+
+#include <opencv2/core/core.hpp>
+
+
+namespace Network {
+namespace CreateNetwork {
+
+/*!
+ * \class CreateNetwork
+ * \brief Class used to build Bayes network
+ * \author kkaterza
+ */
+class CreateNetwork: public Base::Component
+{
+public:
+	/*!
+	 * Constructor.
+	 */
+    CreateNetwork(const std::string & name = "");
+
+	/*!
+	 * Destructor
+	 */
+    virtual ~CreateNetwork();
+
+	/*!
+	 * Prepare data streams and handlers
+	 */
+	void prepareInterface();
+
+protected:
+
+	/*!
+	 * Connects source to given device.
+	 */
+	bool onInit();
+
+	/*!
+	 * Disconnect source from device, closes streams, etc.
+	 */
+	bool onFinish();
+
+	/*!
+	 * Start component
+	 */
+	bool onStart();
+
+	/*!
+	 * Stop component
+	 */
+    bool onStop();
+
+    /*!
+     *
+     */
+    bool onStep();
+
+	/*!
+	 * Event handler function.
+	 */
+	void onNewImage();
+
+	/// Input data stream
+	Base::DataStreamIn<cv::Mat> in_img;
+
+	/// Output data stream - image with drawn blobs
+	Base::DataStreamOut<cv::Mat> out_img;
+
+private:
+	cv::Mat img_uchar;
+
+};
+
+}//: namespace CreateNetwork
+}//: namespace Network
+
+
+/*
+ * Register processor component.
+ */
+REGISTER_COMPONENT("CreateNetwork", Network::CreateNetwork::CreateNetwork)
+
+#endif /* CREATE_NETWORK_HPP_ */
+
