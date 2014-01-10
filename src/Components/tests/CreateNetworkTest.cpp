161c45151cbd45335c3aacadecdfafb4f7249eeb
qiubix <qiubix@gmail.com>
2014-01-10 15:22:49 +0100

    Enable unit testing
    Add directory for tests.
    Import boost testing framework in CMakeLists.txt file.
    Add first test for CreateNetwork component.
    
    DOES NOT COMPILE!!!

---
+++ src/Components/tests/CreateNetworkTest.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE CreateNetworkTest
+#include <boost/test/included/unit_test.hpp>
+
+#include "CreateNetwork.hpp"
+
+BOOST_AUTO_TEST_CASE( test )
+{
+    Processors::Network::CreateNetwork* network = new Processors::Network::CreateNetwork("TestNetwork");
+    BOOST_CHECK_EQUAL(2, 2);
+}
