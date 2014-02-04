d171a1e06b13536a57d288813d38979f0b73d617
qiubix <qiubix@gmail.com>
2014-02-04 21:32:46 +0100

    Match corresponding features.

--- src/Components/SIFTFeatureMatcher/SIFTFeatureMatcher.cpp
+++ src/Components/SIFTFeatureMatcher/SIFTFeatureMatcher.cpp
@@ -15,9 +15,38 @@
 
 #include <boost/thread.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <pcl/point_representation.h>
+
+#include "pcl/impl/instantiate.hpp"
+#include "pcl/search/kdtree.h"
+#include "pcl/search/impl/kdtree.hpp"
+#include <pcl/registration/correspondence_estimation.h>
+#include "pcl/registration/correspondence_rejection_sample_consensus.h"
 
 namespace Processors {
 namespace Network {
+
+class SIFTFeatureRepresentation: public pcl::DefaultFeatureRepresentation <PointXYZSIFT> //could possibly be pcl::PointRepresentation<...> ??
+{
+	using pcl::PointRepresentation<PointXYZSIFT>::nr_dimensions_;
+	public:
+	SIFTFeatureRepresentation ()
+	{
+		// Define the number of dimensions
+		nr_dimensions_ = 128 ;
+		trivial_ = false ;
+	}
+
+	// Override the copyToFloatArray method to define our feature vector
+	virtual void copyToFloatArray (const PointXYZSIFT &p, float * out) const
+	{
+		//This representation is only for determining correspondences (not for use in Kd-tree for example - so use only SIFT part of the point	
+		for (register int i = 0; i < 128 ; i++)
+			out[i] = p.descriptor[i];//p.descriptor.at<float>(0, i) ;
+		//std::cout << "SIFTFeatureRepresentation:copyToFloatArray()" << std::endl ;
+	}
+};
+
 
 SIFTFeatureMatcher::SIFTFeatureMatcher(const std::string & name) : Base::Component(name)
 {
@@ -82,10 +111,66 @@ void SIFTFeatureMatcher::onJointCloud()
 
 void SIFTFeatureMatcher::onInstances()
 {
-//    pcl::PointCloud<PointXYZSIFT>::Ptr instance = in_instances.read();
+    if(jointCloud == NULL) {
+        return;
+    }
+    
     std::vector <AbstractObject*> instances = in_instances.read();
     pcl::PointCloud<PointXYZSIFT>::Ptr instance = dynamic_cast<SIFTObjectModel*>(instances.at(0))->cloud_xyzsift;
-    
+    LOG(LINFO) << "Instance cloud size: " << instance -> size();
+
+	pcl::CorrespondencesPtr correspondences(new pcl::Correspondences()) ;
+	pcl::registration::CorrespondenceEstimation<PointXYZSIFT, PointXYZSIFT> correst ;
+
+	SIFTFeatureRepresentation::Ptr point_representation(new SIFTFeatureRepresentation()) ;
+	correst.setPointRepresentation(point_representation) ;
+	correst.setInputSource(jointCloud) ;
+	correst.setInputTarget(instance) ;
+	correst.determineReciprocalCorrespondences(*correspondences) ;
+
+	LOG(LDEBUG) << "Correspondences determined " << correspondences -> size();
+
+	if ( correspondences -> size() > 4 ) {
+		//ransac znalezienie blednych dopasowan
+		pcl::Correspondences inliers ;
+		pcl::registration::CorrespondenceRejectorSampleConsensus<PointXYZSIFT> sac ;
+		sac.setInputSource(jointCloud) ;
+		sac.setInputTarget(instance) ;
+		sac.setInlierThreshold(0.001f) ;
+		sac.setMaximumIterations(2000) ;
+		sac.setInputCorrespondences(correspondences) ;
+		sac.getCorrespondences(inliers);
+
+		LOG(LDEBUG) << "Wrong matches found";
+
+		//usuniecie blednych dopasowan
+		pcl::Correspondences::iterator iter_inliers = inliers.begin();
+		while(iter_inliers!=inliers.end()) {
+			pcl::Correspondences::iterator iter_correspondences = correspondences->begin();
+			while(iter_correspondences!=correspondences->end()) {
+				if(iter_correspondences->index_query == iter_inliers->index_query) {
+					iter_correspondences= correspondences->erase(iter_correspondences);
+					break;
+				}
+				else {
+					++iter_correspondences;
+				}
+			}
+			++iter_inliers;	
+		}
+	}
+
+	LOG(LINFO) << "Number of reciprocal correspondences: " << correspondences->size() << " out of " << jointCloud->size() << " keypoints";
+
+	for(int i = 0; i< correspondences->size();i++){	
+		if (correspondences->at(i).index_query >=jointCloud->size() ||
+		        correspondences->at(i).index_match >=instance->size()){
+			continue;
+		}
+        int featureIndex = correspondences -> at(i).index_query;
+        featuresIndexes.push_back(featureIndex);
+        LOG(LINFO) << "Index of matching feature: " << featureIndex;
+	}
 }
 
 }//: namespace Network
