a5e246759bccb55a4703a852ac1cec3d3d7642c3
qiubix <qiubix@gmail.com>
2014-10-05 16:45:16 +0200

    Define BayesNetwork interface
    by adding public methods with empty bodies

--- src/Components/CreateNetworkWithSpacialDependencies/BayesNetwork.cpp
+++ src/Components/CreateNetworkWithSpacialDependencies/BayesNetwork.cpp
@@ -1,6 +1,52 @@
 #include "BayesNetwork.hpp"
+#include "Logger.hpp"
 
 void Processors::Network::BayesNetwork::addVoxelNode(int id)
 {
-  
+  LOG(LTRACE) << "Adding voxel node to network";
+}
+
+std::string Processors::Network::BayesNetwork::createVoxelName(int id)
+{
+  LOG(LTRACE) << "Creating voxel name";
+}
+
+std::string Processors::Network::BayesNetwork::createFeatureName(int id)
+{
+  LOG(LTRACE) << "Creating feature name";
+}
+
+void Processors::Network::BayesNetwork::addArc(std::string parentName, std::string childName)
+{
+  LOG(LTRACE) << "Adding arc between nodes: " << parentName << "->" << childName;
+}
+
+void Processors::Network::BayesNetwork::setCPTofAllNodes()
+{
+  LOG(LTRACE) << "Setting CPTs of all nodes in network";
+}
+
+void Processors::Network::BayesNetwork::setNodeCPT(std::string name, int numberOfParents)
+{
+  LOG(LTRACE) << "Setting CPT of node " << name;
+}
+
+void Processors::Network::BayesNetwork::fillCPT(std::string name, std::vector<double> probabilities)
+{
+  LOG(LTRACE) << "Filling CPT of node " << name;
+}
+
+int Processors::Network::BayesNetwork::generateNext(std::basic_string::iterator start, std::basic_string::iterator end)
+{
+  // Generate next string of 1 and 0 to find next cell in CPT
+}
+
+std::string Processors::Network::BayesNetwork::getNodeName(int nodeHandle)
+{
+  LOG(LTRACE) << "Get name of node: " << nodeHandle;
+}
+
+int Processors::Network::BayesNetwork::getNumberOfChildren(int nodeId)
+{
+  LOG(LTRACE) << "Get number of children of node nr: " << nodeId;
 }
--- src/Components/CreateNetworkWithSpacialDependencies/BayesNetwork.hpp
+++ src/Components/CreateNetworkWithSpacialDependencies/BayesNetwork.hpp
@@ -10,7 +10,22 @@ class BayesNetwork {
 public:
     BayesNetwork() {}
     ~BayesNetwork() {}
+    
+    //building network
     void addVoxelNode(int id);
+    string createVoxelName(int id);
+    string createFeatureName(int id);
+    void addArc(std::string parentName, std::string childName);
+    
+    //manipulating CPTs
+    void setCPTofAllNodes();
+    void setNodeCPT(std::string name, int numberOfParents);
+    void fillCPT(std::string name, std::vector<double> probabilities);
+    int generateNext(std::string::iterator start, std::string::iterator end);
+    
+    //getters
+    std::string getNodeName(int nodeHandle);
+    int getNumberOfChildren(int nodeId);
 private:
     DSL_network network;
 
