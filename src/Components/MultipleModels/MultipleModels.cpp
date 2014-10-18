4fd24b830de5cf3f3b24d3974c8ee2065dd5529c
qiubix <qiubix@gmail.com>
2014-10-18 23:59:55 +0200

    Fix indent

--- src/Components/MultipleModels/MultipleModels.cpp
+++ src/Components/MultipleModels/MultipleModels.cpp
@@ -17,56 +17,57 @@ namespace Network {
 
 MultipleModels::MultipleModels(const std::string & name) : Base::Component(name)
 {
-    LOG(LTRACE)<<"Hello MultipleModels\n";
+  LOG(LTRACE)<<"Hello MultipleModels\n";
 }
 
 MultipleModels::~MultipleModels()
 {
-    LOG(LTRACE)<<"Good bye MultipleModels\n";
+  LOG(LTRACE)<<"Good bye MultipleModels\n";
 }
 
 void MultipleModels::prepareInterface()
 {
-	LOG(LTRACE) << "MultipleModels::prepareInterface\n";
+  LOG(LTRACE) << "MultipleModels::prepareInterface\n";
 
-	// Register data streams.
-	registerStream("in_networks", &in_networks);
-	
-	// Register handlers
-	h_onNetworks.setup(boost::bind(&MultipleModels::createGrid, this));
-	registerHandler("createGrid", &h_onNetworks);
+  // Register data streams.
+  registerStream("in_networks", &in_networks);
 
-	// Register output data stream
-	registerStream("out_probabilities", &out_probabilities);
+  // Register handlers
+  h_onNetworks.setup(boost::bind(&MultipleModels::createGrid, this));
+  registerHandler("createGrid", &h_onNetworks);
+
+  // Register output data stream
+  registerStream("out_probabilities", &out_probabilities);
 }
 
 bool MultipleModels::onInit()
 {
-    LOG(LTRACE) << "MultipleModels::initialize\n";
-    return true;
+  LOG(LTRACE) << "MultipleModels::initialize\n";
+  return true;
 }
 
 bool MultipleModels::onFinish()
 {
-    LOG(LTRACE) << "MultipleModels::finish\n";
-    return true;
+  LOG(LTRACE) << "MultipleModels::finish\n";
+  return true;
+}
+
+bool MultipleModels::onStart()
+{
+  LOG(LTRACE) << "MultipleModels::onStart\n";
+  return true;
 }
 
 bool MultipleModels::onStop()
 {
-    LOG(LTRACE) << "MultipleModels::onStop\n";
-    return true;
+  LOG(LTRACE) << "MultipleModels::onStop\n";
+  return true;
 }
 
 void MultipleModels::createGrid()
 {
   //TODO: creating grid of networks 
-}
-
-bool MultipleModels::onStart()
-{
-    LOG(LTRACE) << "MultipleModels::onStart\n";
-    return true;
+  LOG(LTRACE) << "Creating grid of networks";
 }
 
 }//: namespace Network
