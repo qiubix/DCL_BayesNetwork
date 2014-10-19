202eca0808abcd5616cc607c616ffa26fce43fe9
qiubix <qiubix@gmail.com>
2014-10-19 19:35:50 +0200

    Add new empty component for testing

---
+++ src/Components/TestNetwork/TestNetwork.cpp
@@ -0,0 +1,65 @@
+/*!
+ * \file TestNetwork.cpp
+ * \brief
+ */
+
+#include <memory>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <time.h>
+#include <Common/Timer.hpp>
+
+#include "TestNetwork.hpp"
+
+#include "Logger.hpp"
+#include "Common/Timer.hpp"
+
+#include <boost/thread.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+namespace Processors {
+namespace Network {
+
+TestNetwork::TestNetwork(const std::string & name) : Base::Component(name)
+{
+    LOG(LTRACE)<<"Hello TestNetwork";
+}
+
+TestNetwork::~TestNetwork()
+{
+    LOG(LTRACE)<<"Good bye TestNetwork";
+}
+
+void TestNetwork::prepareInterface()
+{
+    LOG(LTRACE) << "TestNetwork::prepareInterface";
+}
+
+bool TestNetwork::onInit()
+{
+    LOG(LTRACE) << "TestNetwork::initialize";
+    return true;
+}
+
+bool TestNetwork::onFinish()
+{
+    LOG(LTRACE) << "TestNetwork::finish";
+    return true;
+}
+
+bool TestNetwork::onStart()
+{
+    LOG(LTRACE) << "TestNetwork::onStart";
+    return true;
+}
+
+bool TestNetwork::onStop()
+{
+    LOG(LTRACE) << "TestNetwork::onStop";
+    return true;
+}
+
+
+}//: namespace Network
+}//: namespace Processors
---
+++ src/Components/TestNetwork/TestNetwork.hpp
@@ -0,0 +1,87 @@
+/*!
+ * \file TestNetwork.hpp
+ * \brief
+ */
+
+#ifndef TEST_NETWORK_HPP_
+#define TEST_NETWORK_HPP_
+
+#define CV_NO_BACKWARD_COMPATIBILITY
+
+
+#include "Component_Aux.hpp"
+#include "Component.hpp"
+#include "DataStream.hpp"
+#include "Property.hpp"
+
+namespace Processors {
+namespace Network {
+
+/*!
+ * \class TestNetwork
+ * \brief Component for testing purposes
+ * \author Karol Katerżawa
+ */
+class TestNetwork: public Base::Component
+{
+public:
+    /*!
+     * Constructor.
+     */
+    TestNetwork(const std::string & name = "TestNetwork");
+
+    /*!
+     * Destructor
+     */
+    virtual ~TestNetwork();
+
+    /*!
+     * Prepare data streams and handlers
+     */
+    void prepareInterface();
+
+protected:
+    ///Input data streams
+    
+    //Output data streams
+
+    /*!
+     * Connects source to given device.
+     */
+    bool onInit();
+
+    /*!
+     * Disconnect source from device, closes streams, etc.
+     */
+    bool onFinish();
+
+    /*!
+     * Start component
+     */
+    bool onStart();
+
+    /*!
+     * Stop component
+     */
+    bool onStop();
+
+    /// Event handlers
+
+    /*!
+     * Event handler function.
+     */
+
+private:
+};
+
+}//: namespace Network
+}//: namespace Processors
+
+
+/*
+ * Register processor component.
+ */
+REGISTER_COMPONENT("TestNetwork", Processors::Network::TestNetwork)
+
+#endif /* TEST_NETWORK_HPP_ */
+
