00afed2d9840b3f7834b8d42cea1bc18ccf0624c
qiubix <qiubix@gmail.com>
2014-12-01 12:04:53 +0100

    Fix gtest include, make SMILE available on top level

--- test/BayesNetworkTest.cpp
+++ test/BayesNetworkTest.cpp
@@ -1,11 +1,11 @@
 #include <gmock/gmock.h>
 using ::testing::Eq;
-#include <gmock/gtest.h>
+#include <gtest/gtest.h>
 using ::testing::Test;
 
 class BayesNetworkTest : public Test {
   protected:
-}
+};
 
 TEST_F(BayesNetworkTest, shouldTestNothing)
 {
