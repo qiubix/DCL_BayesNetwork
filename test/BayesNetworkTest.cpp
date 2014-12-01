d536994bed06bae3556b55e096cd8717dd8be1cd
qiubix <qiubix@gmail.com>
2014-12-01 10:03:08 +0100

    Add first dummy test

---
+++ test/BayesNetworkTest.cpp
@@ -0,0 +1,13 @@
+#include <gmock/gmock.h>
+using ::testing::Eq;
+#include <gmock/gtest.h>
+using ::testing::Test;
+
+class BayesNetworkTest : public Test {
+  protected:
+}
+
+TEST_F(BayesNetworkTest, shouldTestNothing)
+{
+  EXPECT_TRUE(true);
+}
