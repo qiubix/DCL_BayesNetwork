f9fe2b7248266cff3c165f4159057f618258e9c6
qiubix <qiubix@gmail.com>
2014-12-03 23:17:53 +0100

    Add simple test using BayesNetwork class

--- test/BayesNetworkTest.cpp
+++ test/BayesNetworkTest.cpp
@@ -3,11 +3,21 @@ using ::testing::Eq;
 #include <gtest/gtest.h>
 using ::testing::Test;
 
+#include "../src/Components/CreateNetworkWithSpacialDependencies/BayesNetwork.hpp"
+
 class BayesNetworkTest : public Test {
   protected:
+    Processors::Network::BayesNetwork network;
 };
 
 TEST_F(BayesNetworkTest, shouldTestNothing)
 {
   EXPECT_TRUE(true);
 }
+
+TEST_F(BayesNetworkTest, shouldGetNumberOfNodesInNetwork)
+{
+  int numberOfNodes = network.getNumberOfNodes();
+  EXPECT_EQ(numberOfNodes, 0);
+}
+
