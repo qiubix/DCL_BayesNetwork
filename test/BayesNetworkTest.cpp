26ca53822a68b88545d0c1d4d437c7cc8a173c40
qiubix <qiubix@gmail.com>
2014-12-11 14:16:33 +0100

    Add descriptions to tests, explaining specific test cases

--- test/BayesNetworkTest.cpp
+++ test/BayesNetworkTest.cpp
@@ -36,15 +36,39 @@ TEST_F(BayesNetworkTest, shouldAddNodeToEmptyNetwork)
 
 TEST_F(BayesNetworkTest, shouldAddFeatureNodeToNetwork)
 {
+  /*
+   * create network with some nodes
+   * add feature node
+   * check if name correct
+   * check if node is in the right layer
+   * check if node has proper CPT
+   */
   EXPECT_TRUE(true);
 }
 
 TEST_F(BayesNetworkTest, shouldConnectTwoNodes)
 {
+  /*
+   * create network with node
+   * add new node to network
+   * connect two nodes
+   * check if nodes connected
+   * check number of children
+   * check number of parents
+   * check size of CPT
+   */
   EXPECT_TRUE(true);
 }
 
 TEST_F(BayesNetworkTest, shouldFillNodeCPT)
 {
+  /*
+   * create network with few nodes
+   * connect those nodes
+   * check size of child node's CPT
+   * fill node's CPT
+   * check whether every cell has been updated
+   * check whether every cell has proper value
+   */
   EXPECT_TRUE(true);
 }
