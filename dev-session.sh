09acf835e02b4ba2743df1fd471a275c76e6c33f
qiubix <qiubix@gmail.com>
2015-03-12 08:43:16 +0100

    Fix tmux script
    ensure 256 color terminal

--- dev-session.sh
+++ dev-session.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+alias tmux='TERM=xterm-256color /home/$USER/bin/tmux -2'
+echo "Check whether session thesis-dev exists"
 tmux has-session -t thesis-dev
-if [ $?  != 0 ]
+if [ $? != 0 ]
 then
+  echo "Session thesis-dev doesn't exist. Creating new one"
   tmux new-session -s thesis-dev -n vim -d
   tmux send-keys -t thesis-dev 'cd ~/DCL/BayesNetwork/build' C-m
   tmux send-keys -t thesis-dev 'vim' C-m
@@ -12,6 +15,8 @@ then
   tmux send-keys -t thesis-dev:3 'cd ~/DCL/BayesNetwork/build' C-m
   tmux new-window -n browse -t thesis-dev
   tmux send-keys -t thesis-dev:4 'cd ~/DCL/' C-m
+  echo "Select starting window"
   tmux select-window -t thesis-dev:1
 fi
-#tmux -2 attach -t thesis-dev
+echo "Attaching to thesis-dev session..."
+tmux attach -t thesis-dev
