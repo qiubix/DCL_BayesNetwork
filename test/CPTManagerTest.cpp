62d7446fbb7686fc662dcbd686e189adec9caf81
qiubix <qiubix@gmail.com>
2015-03-16 14:48:08 +0100

    Set up first test

--- test/CPTManagerTest.cpp
+++ test/CPTManagerTest.cpp
@@ -4,6 +4,7 @@ using ::testing::Eq;
 using ::testing::Test;
 
 #include "../src/Components/NetworkBuilder/BayesNetwork.hpp"
+#include "../src/Components/NetworkBuilder/CPTManager.hpp"
 #include "../src/Components/NetworkBuilder/BayesNetworkNode.hpp"
 
 using namespace Processors::Network;
@@ -12,7 +13,20 @@ class CPTManagerTest : public Test {
 
 };
 
-TEST_F(CPTManagerTest, shouldTestNothing)
+TEST_F(CPTManagerTest, shouldSetCPTOfTheNodeWithoutChildren)
+{
+  DSL_network network;
+  int nodeHandle = network.AddNode(DSL_CPT, "Node1");
+  DSL_node* node = network.GetNode(nodeHandle);
+  DSL_stringArray outcomes;
+  outcomes.Add("true");
+  outcomes.Add("false");
+  node->Definition()->SetNumberOfOutcomes(outcomes);
+  CPTManager manager(node);
+  ASSERT_TRUE(true);
+}
+
+TEST_F(CPTManagerTest, shouldSetCPTOfTheNodeWithChildren)
 {
   ASSERT_TRUE(true);
 }
