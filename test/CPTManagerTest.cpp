2ebe37bd62b3c2eb599b3947248e6497cdf90096
qiubix <qiubix@gmail.com>
2015-03-18 21:24:53 +0100

    Start createNodeWithCPT method for test preparation
    draft new test for displaying node's CPT

--- test/CPTManagerTest.cpp
+++ test/CPTManagerTest.cpp
@@ -14,8 +14,10 @@ public:
   CPTManagerTest() {
     network = new DSL_network();
   }
+
   ~CPTManagerTest() {}
-  DSL_node* createNewNode() {
+
+  DSL_node* createNodeWithoutCPT() {
     int nodeHandle = network->AddNode(DSL_CPT, "Node1");
     DSL_node* node = network->GetNode(nodeHandle);
     DSL_stringArray outcomes;
@@ -24,18 +26,30 @@ public:
     node->Definition()->SetNumberOfOutcomes(outcomes);
     return node;
   }
+
+  DSL_node* createNodeWithCPT() {
+    DSL_node* node = createNodeWithoutCPT();
+    return node;
+  }
+
   std::vector<double> getNodeCPT(DSL_node* node) {
     DSL_Dmatrix* matrix = node->Definition()->GetMatrix();
     std::vector<double> probs;
     return probs;
   }
+
 protected:
   DSL_network* network;
 };
 
+TEST_F(CPTManagerTest, shouldDisplayCPTOfTheNode)
+{
+  DSL_node* node = createNodeWithCPT();
+}
+
 TEST_F(CPTManagerTest, shouldSetCPTOfTheNodeWithoutChildren)
 {
-  DSL_node* node = createNewNode();
+  DSL_node* node = createNodeWithoutCPT();
   ASSERT_EQ(2, node->Definition()->GetSize());
   CPTManager manager(node);
   ASSERT_TRUE(true);
