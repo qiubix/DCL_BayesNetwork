2fb0eb948ee7f2cb35ab3b0ce9b8da7493126bc3
qiubix <qiubix@gmail.com>
2015-03-29 17:50:54 +0200

    theCoordinates->coordinates

--- src/Components/NetworkBuilder/CPTManager.cpp
+++ src/Components/NetworkBuilder/CPTManager.cpp
@@ -25,16 +25,16 @@ void CPTManager::fillCPT(std::vector<double> probabilities)
 {
   std::string name(node->Info().Header().GetId());
   LOG(LTRACE) << "Filling CPT of node " << name;
-  DSL_sysCoordinates theCoordinates(*node->Definition());
+  DSL_sysCoordinates coordinates(*node->Definition());
 
   std::vector<double>::iterator it = probabilities.begin();
   //TODO: switch do-while to for.
   // this is somehow problematic because Next() method automatically increments coordinates
   do {
-    theCoordinates.UncheckedValue() = *it;
+    coordinates.UncheckedValue() = *it;
     LOG(LTRACE) << "Probability: " << *it;
     ++it;
-  } while(theCoordinates.Next() != DSL_OUT_OF_RANGE || it != probabilities.end());
+  } while(coordinates.Next() != DSL_OUT_OF_RANGE || it != probabilities.end());
 }
 
 } //: namespace Network
