406b01f87ecf02207f9f6a6118cd259e783dc85d
qiubix <qiubix@gmail.com>
2015-04-04 17:49:15 +0200

    Fix paths in tmux script

--- dev-session.sh
+++ dev-session.sh
@@ -7,24 +7,24 @@ if [ $? != 0 ]
 then
   echo "Session thesis-dev doesn't exist. Creating new one"
   tmux new-session -s thesis-dev -n vim -d
-  tmux send-keys -t thesis-dev 'cd ~/DCL/BayesNetwork/build' C-m
+  tmux send-keys -t thesis-dev 'cd ~/src/DCL/BayesNetwork/build' C-m
   tmux send-keys -t thesis-dev 'vim' C-m
 
   tmux new-window -n console -t thesis-dev
-  tmux send-keys -t thesis-dev:2 'cd ~/DCL/BayesNetwork' C-m
+  tmux send-keys -t thesis-dev:2 'cd ~/src/DCL/BayesNetwork' C-m
 
   tmux new-window -n test -t thesis-dev
   tmux split-window -h
-  tmux send-keys -t thesis-dev:3.1 'cd ~/DCL/BayesNetwork/build' C-m
-  tmux send-keys -t thesis-dev:3.2 'cd ~/DCL/BayesNetwork/build/test' C-m
+  tmux send-keys -t thesis-dev:3.1 'cd ~/src/DCL/BayesNetwork/build' C-m
+  tmux send-keys -t thesis-dev:3.2 'cd ~/src/DCL/BayesNetwork/build/test' C-m
   tmux send-keys -t thesis-dev:3.1 'make test' C-m
   tmux send-keys -t thesis-dev:3.2 'find | grep Test$' C-m
 
   tmux new-window -n discode -t thesis-dev
-  tmux send-keys -t thesis-dev:4 'cd ~/DCL/BayesNetwork/build' C-m
+  tmux send-keys -t thesis-dev:4 'cd ~/src/DCL/BayesNetwork/build' C-m
 
   tmux new-window -n browse -t thesis-dev
-  tmux send-keys -t thesis-dev:5 'cd ~/DCL/' C-m
+  tmux send-keys -t thesis-dev:5 'cd ~/src/DCL/' C-m
 
   echo "Select starting window"
   tmux select-window -t thesis-dev:1
