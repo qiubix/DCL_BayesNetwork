0b9b8ac8d5e02c47a9afd2eb55b8fec7fe5454c7
qiubix <qiubix@gmail.com>
2015-04-27 16:13:42 +0200

    Create octree with iterator, init node

--- test/OctreeNodeTest.cpp
+++ test/OctreeNodeTest.cpp
@@ -3,13 +3,28 @@ using ::testing::Eq;
 #include <gtest/gtest.h>
 using ::testing::Test;
 
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+
 #include "../src/Components/NetworkBuilder/Octree.hpp"
 #include "../src/Components/NetworkBuilder/OctreeNode.hpp"
 #include "../src/Components/NetworkBuilder/OctreeBranchNode.hpp"
 #include "../src/Components/NetworkBuilder/OctreeLeafNode.hpp"
+#include "../src/Components/NetworkBuilder/OctreeContainers.hpp"
 
 
 TEST(OctreeNodeTest, shouldInitWithPclOctreeNode) {
+  pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
+  if (pcl::io::loadPCDFile<PointXYZSIFT> ("test_cloud.pcd", *cloud) == -1) {
+    std::cout <<"Error reading file!\n";
+  }
+  typedef pcl::octree::OctreePointCloud<PointXYZSIFT, Processors::Network::OctreeContainerPointIndicesWithId, Processors::Network::OctreeContainerEmptyWithId> Octree;
+  Octree octree(128.0f);
+  octree.setInputCloud(cloud);
+  octree.addPointsFromInputCloud();
+  Octree::DepthFirstIterator it = octree.depth_begin();
+  Processors::Network::OctreeNode node = it.getCurrentOctreeNode();
   ASSERT_TRUE(true);
 }
 
