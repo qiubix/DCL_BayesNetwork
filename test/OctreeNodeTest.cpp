fdc4d9885dc1df678001323aa6b233267f175602
qiubix <qiubix@gmail.com>
2015-04-27 14:31:04 +0200

    Add test suite for OctreeNode

---
+++ test/OctreeNodeTest.cpp
@@ -0,0 +1,14 @@
+#include <gmock/gmock.h>
+using ::testing::Eq;
+#include <gtest/gtest.h>
+using ::testing::Test;
+
+#include "../src/Components/NetworkBuilder/Octree.hpp"
+#include "../src/Components/NetworkBuilder/OctreeNode.hpp"
+#include "../src/Components/NetworkBuilder/OctreeBranchNode.hpp"
+#include "../src/Components/NetworkBuilder/OctreeLeafNode.hpp"
+
+
+TEST(OctreeNodeTest, shouldTestNothing) {
+  ASSERT_TRUE(true);
+}
