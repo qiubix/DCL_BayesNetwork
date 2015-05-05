a443799b5fb3e9703fe343f6585b9bde8f375c37
qiubix <qiubix@gmail.com>
2015-05-05 12:58:32 +0200

    Cleanup OctreeBranchNodeTest fixture class

--- test/OctreeNodeTest.cpp
+++ test/OctreeNodeTest.cpp
@@ -103,24 +103,9 @@ class OctreeBranchNodeTest : public Test
 public:
   typedef pcl::octree::OctreePointCloud<PointXYZSIFT, Processors::Network::OctreeContainerPointIndicesWithId, Processors::Network::OctreeContainerEmptyWithId> Octree;
 
-  OctreeBranchNodeTest() : octree(128.0f) {
-  }
+  OctreeBranchNodeTest() {}
 
   ~OctreeBranchNodeTest() {}
-
-  void readPointCloudFromFile() {
-    pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
-    if (pcl::io::loadPCDFile<PointXYZSIFT> ("test_cloud.pcd", *cloud) == -1) {
-      std::cout <<"Error reading file!\n";
-    }
-    octree.setInputCloud(cloud);
-    octree.addPointsFromInputCloud();
-  }
-
-  Octree::DepthFirstIterator getSampleOctreeFirstNode() {
-    Octree::DepthFirstIterator it = octree.depth_begin();
-    return it;
-  }
 
   Octree createOctreeWithOnePoint() {
     pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
@@ -139,8 +124,27 @@ public:
     return octree;
   }
 
-protected:
-  Octree octree;
+  Octree createOctreeWithTwoPoints() {
+    pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
+    cloud->width = 2;
+    cloud->height = 1;
+    cloud->points.resize(cloud->width * cloud->height);
+    cloud->points[0].x = 0.1;
+    cloud->points[0].y = 0.2;
+    cloud->points[0].z = 0.3;
+    cloud->points[1].x = 1.1;
+    cloud->points[1].y = 1.2;
+    cloud->points[1].z = 1.3;
+    for(int i=0; i<128; i++) {
+      cloud->points[0].descriptor[i] = i;
+      cloud->points[1].descriptor[i] = i;
+    }
+
+    Octree octree(128.0f);
+    octree.setInputCloud(cloud);
+    octree.addPointsFromInputCloud();
+    return octree;
+  }
 };
 
 TEST_F(OctreeBranchNodeTest, shouldReturnTrueIfNextNodeIsAlsoBranchNode) {
@@ -168,24 +172,7 @@ TEST_F(OctreeBranchNodeTest, shouldHaveOnlyOneChild) {
 }
 
 TEST_F(OctreeBranchNodeTest, shouldHaveMultipleChildren) {
-  pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
-  cloud->width = 2;
-  cloud->height = 1;
-  cloud->points.resize(cloud->width * cloud->height);
-  cloud->points[0].x = 0.1;
-  cloud->points[0].y = 0.2;
-  cloud->points[0].z = 0.3;
-  cloud->points[1].x = 1.1;
-  cloud->points[1].y = 1.2;
-  cloud->points[1].z = 1.3;
-  for(int i=0; i<128; i++) {
-    cloud->points[0].descriptor[i] = i;
-    cloud->points[1].descriptor[i] = i;
-  }
-
-  Octree octree(128.0f);
-  octree.setInputCloud(cloud);
-  octree.addPointsFromInputCloud();
+  Octree octree = createOctreeWithTwoPoints();
   Octree::DepthFirstIterator it = octree.depth_begin();
 
   Processors::Network::OctreeBranchNode node = it.getCurrentOctreeNode();
@@ -195,14 +182,7 @@ TEST_F(OctreeBranchNodeTest, shouldHaveMultipleChildren) {
 }
 
 TEST_F(OctreeBranchNodeTest, shouldSetId) {
-  typedef pcl::octree::OctreePointCloud<PointXYZSIFT, Processors::Network::OctreeContainerPointIndicesWithId, Processors::Network::OctreeContainerEmptyWithId> Octree;
-  pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
-  if (pcl::io::loadPCDFile<PointXYZSIFT> ("test_cloud.pcd", *cloud) == -1) {
-    std::cout <<"Error reading file!\n";
-  }
-  Octree octree(128.0f);
-  octree.setInputCloud(cloud);
-  octree.addPointsFromInputCloud();
+  Octree octree = createOctreeWithOnePoint();
   Octree::DepthFirstIterator it = octree.depth_begin();
   Processors::Network::OctreeBranchNode node = it.getCurrentOctreeNode();
 
