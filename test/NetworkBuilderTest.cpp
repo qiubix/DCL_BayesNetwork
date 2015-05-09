0b4724e1ac8e0b2448c8ea071f28194849902b00
qiubix <qiubix@gmail.com>
2015-05-09 22:32:17 +0200

    Prepare tests with apropried names

--- test/NetworkBuilderTest.cpp
+++ test/NetworkBuilderTest.cpp
@@ -5,14 +5,46 @@ using ::testing::Test;
 
 #include "../src/Components/NetworkBuilder/NetworkBuilder.hpp"
 
-class NetworkBuilderTest : public Test {
-  protected:
-//    Processors::Network::NetworkBuilder* component = new Processors::Network::NetworkBuilder("name");
-};
-
-TEST_F(NetworkBuilderTest, shouldCreateComponentForTesting)
+TEST(NetworkBuilderTest, shouldCreateComponentForTesting)
 {
   Processors::Network::NetworkBuilder component("name");
   component.prepareInterface();
+  EXPECT_TRUE(true);
+}
+
+TEST(NetworkBuilderTest, shouldReadSIFTPointCloudFromInputDataPort)
+{
+  EXPECT_TRUE(true);
+}
+
+TEST(NetworkBuilderTest, shouldBuildNetworkWithOnlyOneFeatureNode) {
+  EXPECT_TRUE(true);
+}
+
+TEST(NetworkBuilderTest, shouldBuildNetworkWithMultipleFeatureNodes) {
+  EXPECT_TRUE(true);
+}
+
+TEST(NetworkBuilderTest, shouldFillCPTsAcordingToNumberOfParents) {
+  EXPECT_TRUE(true);
+}
+
+TEST(NetworkBuilderTest, shouldSetDefaultProbabilityValuesForFeatureNodes) {
+  EXPECT_TRUE(true);
+}
+
+TEST(NetworkBuilderTest, shouldHaveOnlyOneChildNode) {
+  EXPECT_TRUE(true);
+}
+
+TEST(NetworkBuilderTest, shouldNotHaveCycles) {
+  EXPECT_TRUE(true);
+}
+
+TEST(NetworkBuilderTest, shouldHaveNodesWithUniqueNames) {
+  EXPECT_TRUE(true);
+}
+
+TEST(NetworkBuilderTest, shouldWriteNetworkToOutputPort) {
   EXPECT_TRUE(true);
 }
