b31f5567314e8215cf610d1790625306b9578022
qiubix <qiubix@gmail.com>
2015-05-10 17:53:52 +0200

    Create fixture, remove duplication

--- test/OctreeTest.cpp
+++ test/OctreeTest.cpp
@@ -12,17 +12,43 @@ using ::testing::Test;
 //#include <Types/PointXYZSIFT>
 #include "../src/Types/PointXYZSIFT.hpp"
 
-//TODO: remove duplication in tests - create fixture
+class OctreeTest : public Test {
+public:
+  typedef pcl::PointCloud<PointXYZSIFT>::Ptr PointCloud;
+  PointCloud getPointCloudWithOnePoint() {
+    pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
+    cloud->width = 1;
+    cloud->height = 1;
+    cloud->points.resize(cloud->width * cloud->height);
+    cloud->points[0].x = 0.1;
+    cloud->points[0].y = 0.2;
+    cloud->points[0].z = 0.3;
+    for(int i=0; i<128; i++) cloud->points[0].descriptor[i] = i;
+    return cloud;
+  }
 
-TEST(OctreeTest, shouldInitializeOctreeWithPointCloud) {
-  pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
-  cloud->width = 1;
-  cloud->height = 1;
-  cloud->points.resize(cloud->width * cloud->height);
-  cloud->points[0].x = 0.1;
-  cloud->points[0].y = 0.2;
-  cloud->points[0].z = 0.3;
-  for(int i=0; i<128; i++) cloud->points[0].descriptor[i] = i;
+  PointCloud getPointCloudWithTwoPoints() {
+    pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
+    cloud->width = 2;
+    cloud->height = 1;
+    cloud->points.resize(cloud->width * cloud->height);
+    cloud->points[0].x = 0.1;
+    cloud->points[0].y = 0.2;
+    cloud->points[0].z = 0.3;
+    cloud->points[1].x = 1.1;
+    cloud->points[1].y = 1.2;
+    cloud->points[1].z = 1.3;
+    for(int i=0; i<128; i++) {
+      cloud->points[0].descriptor[i] = i;
+      cloud->points[1].descriptor[i] = i;
+    }
+    return cloud;
+  }
+
+};
+
+TEST_F(OctreeTest, shouldInitializeOctreeWithPointCloud) {
+  PointCloud cloud = getPointCloudWithOnePoint();
   Processors::Network::Octree octree(cloud);
   octree.init();
 
@@ -33,21 +59,8 @@ TEST(OctreeTest, shouldInitializeOctreeWithPointCloud) {
   EXPECT_EQ(1, octreeWithSIFT.getTreeDepth());
 }
 
-TEST(OctreeTest, shouldGetFirstOctreeNode) {
-  pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
-  cloud->width = 2;
-  cloud->height = 1;
-  cloud->points.resize(cloud->width * cloud->height);
-  cloud->points[0].x = 0.1;
-  cloud->points[0].y = 0.2;
-  cloud->points[0].z = 0.3;
-  cloud->points[1].x = 1.1;
-  cloud->points[1].y = 1.2;
-  cloud->points[1].z = 1.3;
-  for(int i=0; i<128; i++) {
-    cloud->points[0].descriptor[i] = i;
-    cloud->points[1].descriptor[i] = i;
-  }
+TEST_F(OctreeTest, shouldGetFirstOctreeNode) {
+  PointCloud cloud = getPointCloudWithTwoPoints();
   Processors::Network::Octree octree(cloud);
   octree.init();
 
@@ -57,15 +70,8 @@ TEST(OctreeTest, shouldGetFirstOctreeNode) {
   ASSERT_EQ(0, it->getCurrentOctreeDepth());
 }
 
-TEST(OctreeTest, shouldGetNextOctreeNodeInDepthSearch) {
-  pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
-  cloud->width = 1;
-  cloud->height = 1;
-  cloud->points.resize(cloud->width * cloud->height);
-  cloud->points[0].x = 0.1;
-  cloud->points[0].y = 0.2;
-  cloud->points[0].z = 0.3;
-  for(int i=0; i<128; i++) cloud->points[0].descriptor[i] = i;
+TEST_F(OctreeTest, shouldGetNextOctreeNodeInDepthSearch) {
+  PointCloud cloud = getPointCloudWithOnePoint();
   Processors::Network::Octree octree(cloud);
   octree.init();
   Processors::Network::Octree::DepthFirstIterator it = octree.depthBegin();
@@ -76,21 +82,8 @@ TEST(OctreeTest, shouldGetNextOctreeNodeInDepthSearch) {
   EXPECT_EQ(1, it->getCurrentOctreeDepth());
 }
 
-TEST(OctreeTest, shouldGetLastOctreeNodeInDepthSearch) {
-  pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
-  cloud->width = 2;
-  cloud->height = 1;
-  cloud->points.resize(cloud->width * cloud->height);
-  cloud->points[0].x = 0.1;
-  cloud->points[0].y = 0.2;
-  cloud->points[0].z = 0.3;
-  cloud->points[1].x = 1.1;
-  cloud->points[1].y = 1.2;
-  cloud->points[1].z = 1.3;
-  for(int i=0; i<128; i++) {
-    cloud->points[0].descriptor[i] = i;
-    cloud->points[1].descriptor[i] = i;
-  }
+TEST_F(OctreeTest, shouldGetLastOctreeNodeInDepthSearch) {
+  PointCloud cloud = getPointCloudWithTwoPoints();
   Processors::Network::Octree octree(cloud);
   octree.init();
   Processors::Network::Octree::DepthFirstIterator it = octree.depthBegin();
