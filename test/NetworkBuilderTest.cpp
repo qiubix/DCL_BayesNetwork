7c43b909a3d180c97b843f925e126398803aeb56
qiubix <qiubix@gmail.com>
2015-05-21 21:46:39 +0200

    Cleanup first test

--- test/NetworkBuilderTest.cpp
+++ test/NetworkBuilderTest.cpp
@@ -7,39 +7,25 @@ using ::testing::Test;
 #include "../src/Components/NetworkBuilder/NetworkBuilder.hpp"
 
 TEST(NetworkBuilderTest, shouldBuildNetworkWithOnlyOneFeatureNode) {
-  /*
-   * init component with one-point cloud
-   * build network
-   * should have one feature node and one voxel node
-   */
   Processors::Network::NetworkBuilder component("name");
   pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
-  cloud->width = 2;
+  cloud->width = 1;
   cloud->height = 1;
   cloud->points.resize(cloud->width * cloud->height);
   cloud->points[0].x = 0.1;
   cloud->points[0].y = 0.2;
   cloud->points[0].z = 0.3;
-  cloud->points[1].x = 1.1;
-  cloud->points[1].y = 1.2;
-  cloud->points[1].z = 1.3;
   for(int i=0; i<128; i++) {
     cloud->points[0].descriptor[i] = i;
-    cloud->points[1].descriptor[i] = 128-i;
   }
-//  if (pcl::io::loadPCDFile<PointXYZSIFT> ("test_cloud.pcd", *cloud) == -1) {
-//    std::cout <<"Error reading file!\n";
-//  }
 
   component.setCloud(cloud);
 
   component.buildNetwork();
-//  ASSERT_TRUE(false);
 
   Processors::Network::BayesNetwork network = component.getNetwork();
   EXPECT_TRUE(network.hasNode("V_0"));
   EXPECT_TRUE(network.hasNode("F_0"));
-  EXPECT_TRUE(true);
 }
 
 TEST(NetworkBuilderTest, shouldBuildNetworkWithMultipleFeatureNodes) {
