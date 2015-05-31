0fff1d1a9372901bf0289b63a8c0313fea237c27
qiubix <qiubix@gmail.com>
2015-05-31 23:31:26 +0200

    Create fixture class

--- test/NetworkBuilderTest.cpp
+++ test/NetworkBuilderTest.cpp
@@ -7,51 +7,55 @@ using ::testing::Test;
 #include "../src/Components/NetworkBuilder/NetworkBuilder.hpp"
 #include "../src/Components/NetworkBuilder/NetworkBuilderExceptions.hpp"
 
-TEST(NetworkBuilderTest, shouldThrowExceptionWhenBuildingFromEmptyCloud) {
-  Processors::Network::NetworkBuilder component("name");
+class NetworkBuilderTest : public Test {
+public:
+  pcl::PointCloud<PointXYZSIFT>::Ptr getPointCloudWithOnePoint() {
+    pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
+    cloud->width = 1;
+    cloud->height = 1;
+    cloud->points.resize(cloud->width * cloud->height);
+    cloud->points[0].x = 0.1;
+    cloud->points[0].y = 0.2;
+    cloud->points[0].z = 0.3;
+    for(int i=0; i<128; i++) {
+      cloud->points[0].descriptor[i] = i;
+    }
+    return cloud;
+  }
+
+};
+
+TEST_F(NetworkBuilderTest, shouldThrowExceptionWhenBuildingFromEmptyCloud) {
+  Processors::Network::NetworkBuilder networkBuilder("name");
   pcl::PointCloud<PointXYZSIFT>::Ptr emptyCloud(new pcl::PointCloud<PointXYZSIFT>);
 
-  EXPECT_THROW(component.buildNetwork(emptyCloud), PointCloudIsEmptyException);
+  EXPECT_THROW(networkBuilder.buildNetwork(emptyCloud), PointCloudIsEmptyException);
 }
 
-pcl::PointCloud<PointXYZSIFT>::Ptr getPointCloudWithOnePoint() {
-  pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
-  cloud->width = 1;
-  cloud->height = 1;
-  cloud->points.resize(cloud->width * cloud->height);
-  cloud->points[0].x = 0.1;
-  cloud->points[0].y = 0.2;
-  cloud->points[0].z = 0.3;
-  for(int i=0; i<128; i++) {
-    cloud->points[0].descriptor[i] = i;
-  }
-  return cloud;
-}
-
-TEST(NetworkBuilderTest, shouldAddHypothesisNodeToNetwork) {
-  Processors::Network::NetworkBuilder component("name");
+TEST_F(NetworkBuilderTest, shouldAddHypothesisNodeToNetwork) {
+  Processors::Network::NetworkBuilder networkBuilder("name");
   pcl::PointCloud<PointXYZSIFT>::Ptr cloud = getPointCloudWithOnePoint();
 
-  component.buildNetwork(cloud);
+  networkBuilder.buildNetwork(cloud);
 
-  Processors::Network::BayesNetwork network = component.getNetwork();
+  Processors::Network::BayesNetwork network = networkBuilder.getNetwork();
   EXPECT_THAT(network.hasNode("V_0"), Eq(true));
 }
 
-TEST(NetworkBuilderTest, shouldBuildNetworkWithOnlyOneFeatureNode) {
-  Processors::Network::NetworkBuilder component("name");
+TEST_F(NetworkBuilderTest, shouldBuildNetworkWithOnlyOneFeatureNode) {
+  Processors::Network::NetworkBuilder networkBuilder("name");
   pcl::PointCloud<PointXYZSIFT>::Ptr cloud = getPointCloudWithOnePoint();
 
-  component.buildNetwork(cloud);
+  networkBuilder.buildNetwork(cloud);
 
-  Processors::Network::BayesNetwork network = component.getNetwork();
+  Processors::Network::BayesNetwork network = networkBuilder.getNetwork();
   ASSERT_THAT(network.hasNode("V_0"), Eq(true));
   ASSERT_THAT(network.hasNode("V_1"), Eq(true));
   ASSERT_THAT(network.hasNode("F_0"), Eq(true));
   ASSERT_THAT(network.getNumberOfNodes(), Eq(3));
 }
 
-TEST(NetworkBuilderTest, shouldBuildNetworkWithMultipleFeatureNodes) {
+TEST_F(NetworkBuilderTest, shouldBuildNetworkWithMultipleFeatureNodes) {
   /*
    * init component with point cloud
    * build network
@@ -61,26 +65,26 @@ TEST(NetworkBuilderTest, shouldBuildNetworkWithMultipleFeatureNodes) {
   EXPECT_TRUE(true);
 }
 
-TEST(NetworkBuilderTest, shouldFillCPTsAcordingToNumberOfParents) {
+TEST_F(NetworkBuilderTest, shouldFillCPTsAcordingToNumberOfParents) {
   EXPECT_TRUE(true);
 }
 
-TEST(NetworkBuilderTest, shouldSetDefaultProbabilityValuesForFeatureNodes) {
+TEST_F(NetworkBuilderTest, shouldSetDefaultProbabilityValuesForFeatureNodes) {
   EXPECT_TRUE(true);
 }
 
-TEST(NetworkBuilderTest, shouldHaveOnlyOneChildNode) {
+TEST_F(NetworkBuilderTest, shouldHaveOnlyOneChildNode) {
   EXPECT_TRUE(true);
 }
 
-TEST(NetworkBuilderTest, shouldNotHaveCycles) {
+TEST_F(NetworkBuilderTest, shouldNotHaveCycles) {
   EXPECT_TRUE(true);
 }
 
-TEST(NetworkBuilderTest, shouldHaveNodesWithUniqueNames) {
+TEST_F(NetworkBuilderTest, shouldHaveNodesWithUniqueNames) {
   EXPECT_TRUE(true);
 }
 
-TEST(NetworkBuilderTest, shouldWriteNetworkToOutputPort) {
+TEST_F(NetworkBuilderTest, shouldWriteNetworkToOutputPort) {
   EXPECT_TRUE(true);
 }
