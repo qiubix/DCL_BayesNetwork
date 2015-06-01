aba9b8404ca3c487d0bf94fa1384c3a85b94c347
qiubix <qiubix@gmail.com>
2015-06-01 12:07:23 +0200

    Test adding multiple feature nodes

--- test/NetworkBuilderTest.cpp
+++ test/NetworkBuilderTest.cpp
@@ -27,6 +27,31 @@ public:
     return cloud;
   }
 
+  pcl::PointCloud<PointXYZSIFT>::Ptr getPointCloudWithThreePoints() {
+    pcl::PointCloud<PointXYZSIFT>::Ptr cloud(new pcl::PointCloud<PointXYZSIFT>);
+    cloud -> width = 3;
+    cloud -> height = 1;
+    cloud -> points.resize(cloud -> width * cloud -> height);
+    cloud -> points[0].x = 0.0;
+    cloud -> points[0].y = 0.0;
+    cloud -> points[0].z = 0.0;
+    cloud -> points[0].pointId = 0;
+    cloud -> points[1].x = 1.0;
+    cloud -> points[1].y = 1.0;
+    cloud -> points[1].z = 1.0;
+    cloud -> points[1].pointId = 2;
+    cloud -> points[2].x = 0.0;
+    cloud -> points[2].y = 0.0;
+    cloud -> points[2].z = 1.0;
+    cloud -> points[2].pointId = 4;
+    for(int i=0; i<128; i++) {
+      cloud -> points[0].descriptor[i] = i;
+      cloud -> points[1].descriptor[i] = 128-i;
+      cloud -> points[2].descriptor[i] = i*i;
+    }
+    return cloud;
+  }
+
   Processors::Network::NetworkBuilder* networkBuilder;
 
 };
@@ -59,13 +84,15 @@ TEST_F(NetworkBuilderTest, shouldBuildNetworkWithOnlyOneFeatureNode) {
 }
 
 TEST_F(NetworkBuilderTest, shouldBuildNetworkWithMultipleFeatureNodes) {
-  /*
-   * init component with point cloud
-   * build network
-   * check if number of feature nodes matches number of points in cloud
-   * check if has only one child
-   */
-  EXPECT_TRUE(true);
+  pcl::PointCloud<PointXYZSIFT>::Ptr cloud = getPointCloudWithThreePoints();
+
+  networkBuilder -> buildNetwork(cloud);
+
+  Processors::Network::BayesNetwork network = networkBuilder -> getNetwork();
+  ASSERT_THAT(network.getNumberOfNodes(), Eq(7));
+  ASSERT_THAT(network.hasNode("F_0"), Eq(true));
+  ASSERT_THAT(network.hasNode("F_2"), Eq(true));
+  ASSERT_THAT(network.hasNode("F_4"), Eq(true));
 }
 
 TEST_F(NetworkBuilderTest, shouldFillCPTsAcordingToNumberOfParents) {
