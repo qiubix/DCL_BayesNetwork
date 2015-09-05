e6ed8cceab35a71135e62351e607ef2b9cd3f90e
qiubix <qiubix@gmail.com>
2015-09-05 21:17:51 +0200

    Add failing test for clearing evidence

--- src/Components/NetworkBuilder/AbstractNetwork.hpp
+++ src/Components/NetworkBuilder/AbstractNetwork.hpp
@@ -7,6 +7,7 @@ public:
   virtual ~AbstractNetwork(){};
 
   virtual double getNodeProbability(const std::string& name) = 0;
+  virtual void clearEvidence() = 0;
 
 private:
   /* data */
--- test/SOMEvaluationTest.cpp
+++ test/SOMEvaluationTest.cpp
@@ -8,10 +8,13 @@ using ::testing::Return;
 #include "../src/Components/NetworkBuilder/BayesNetwork.hpp"
 #include "../src/Components/NetworkBuilder/AbstractNetwork.hpp"
 
+using Processors::Network::SOMEvaluation;
+
 class MockNetwork : public AbstractNetwork
 {
 public:
   MOCK_METHOD1(getNodeProbability, double(const std::string&));
+  MOCK_METHOD0(clearEvidence, void());
 };
 
 TEST(SOMEvaluationTest, shouldDisplayDefaultProbabilityOnStart) {
@@ -19,9 +22,17 @@ TEST(SOMEvaluationTest, shouldDisplayDefaultProbabilityOnStart) {
   EXPECT_CALL(mockNetwork, getNodeProbability("V_0"))
     .WillOnce(Return(0.5));
 
-  Processors::Network::SOMEvaluation evaluator("evaluator");
+  SOMEvaluation evaluator("evaluator");
   evaluator.setNetwork(&mockNetwork);
 
   double hypothesisProbability = evaluator.getNodeProbability(0);
   EXPECT_THAT(hypothesisProbability, Eq(0.5));
+}
+
+TEST(SOMEvaluationTest, shouldClearAllEvidenceOnStart) {
+  SOMEvaluation evaluator("evaluator");
+  MockNetwork mockNetwork;
+  EXPECT_CALL(mockNetwork, clearEvidence());
+  
+  evaluator.evaluate();
 }
