bf2fe782d361e031ec69caea1fb4aef8be80842a
qiubix <qiubix@gmail.com>
2016-05-18 21:28:39 +0200

    Set default build type to Debug

--- cmake/DefaultBuildType.cmake
+++ cmake/DefaultBuildType.cmake
@@ -3,11 +3,11 @@
 
 cmake_minimum_required(VERSION 2.8.12)
 
-macro(setDefaultBuildType BUILD_TYPE)
+function(set_default_build_type BUILD_TYPE)
     if(NOT CMAKE_BUILD_TYPE)
         set (CMAKE_BUILD_TYPE ${BUILD_TYPE})
         message(STATUS "Setting build type to ${BUILD_TYPE}.")
     else(NOT CMAKE_BUILD_TYPE)
         message(STATUS "Build type set to ${CMAKE_BUILD_TYPE} from command line.")
     endif(NOT CMAKE_BUILD_TYPE)
-endmacro(setDefaultBuildType)
+endfunction()
